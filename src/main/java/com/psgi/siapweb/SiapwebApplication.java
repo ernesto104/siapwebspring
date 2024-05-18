a15fa5ccfed68f16e2064546d4b63e01e675447f
ernesto <ernesvz@hotmail.com>
2024-05-18 05:19:04 -0500

    test changes in deploy changing title in menu

--- src/main/java/com/psgi/siapweb/SiapwebApplication.java
+++ src/main/java/com/psgi/siapweb/SiapwebApplication.java
@@ -2,7 +2,7 @@ package com.psgi.siapweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class })
 public class SiapwebApplication {
--- src/main/java/com/psgi/siapweb/controller/TypeDocumentController.java
+++ src/main/java/com/psgi/siapweb/controller/TypeDocumentController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.ModelAndView;
 
 import com.psgi.siapweb.domain.model.TypeDocument;
 import com.psgi.siapweb.domain.service.TypeDocumentService;
