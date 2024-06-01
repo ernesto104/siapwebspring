8485f06f6aa59d63190b2e380a8bd0fc1893546d
ernesto <ernesvz@hotmail.com>
2024-06-01 08:24:16 -0500

    correct btn modificar and view professiones only front

---
+++ src/main/java/com/psgi/siapweb/controller/ProfessionController.java
@@ -0,0 +1,16 @@
+package com.psgi.siapweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfessionController {
+
+    @GetMapping("/professions")
+    public String getControlSystem(Model model){
+        //model.addAttribute("activePage", "maintainMenu");
+        return "professions";
+    }
+
+}
