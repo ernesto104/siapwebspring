8440f5d3cdcd5f9caa57e7b6ad7364b10aaf569a
ernesto <ernesvz@hotmail.com>
2024-06-01 08:04:18 -0500

    nivel educativo view only front

---
+++ src/main/java/com/psgi/siapweb/controller/LevelEducationController.java
@@ -0,0 +1,16 @@
+package com.psgi.siapweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LevelEducationController {
+
+    @GetMapping("/levelEducation")
+    public String getUbigeoPSGI(Model model){
+        //model.addAttribute("activePage", "info");
+        return "levelEducation";
+    }
+
+}
