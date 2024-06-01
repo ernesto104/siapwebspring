93e10fc4bae8cea4cfb4813bb0547a28aedb939b
ernesto <ernesvz@hotmail.com>
2024-06-01 07:49:05 -0500

    add condicionjuridica view only front

---
+++ src/main/java/com/psgi/siapweb/controller/ConditionJuridicaController.java
@@ -0,0 +1,15 @@
+package com.psgi.siapweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConditionJuridicaController {
+
+    @GetMapping("/juridicaCondition")
+    public String getConditionsJuridica(Model model){
+        //model.addAttribute("activePage", "info");
+        return "juridicaCondition";
+    }
+}
